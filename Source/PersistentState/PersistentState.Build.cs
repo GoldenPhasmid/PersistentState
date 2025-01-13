90ce7da5d2872a456280833479ea6eed0df828c9
Oleksandr <okushnirenko.work@gmail.com>
2025-01-13 04:29:31 +0200

    Fix PublicDefinitions being a mess

--- Source/PersistentState/PersistentState.Build.cs
+++ Source/PersistentState/PersistentState.Build.cs
@@ -33,19 +33,19 @@ public class PersistentState : ModuleRules
 		new string[]
 		{
 			// Object IDs are initialized and stored with ObjectName
-			"WITH_OBJECT_NAME = WITH_EDITORONLY_DATA",
+			"WITH_OBJECT_NAME = (WITH_EDITORONLY_DATA)",
 			// Object references are sanitized for being able to restored
-			"WITH_SANITIZE_REFERENCES  = WITH_EDITOR || WITH_EDITORONLY_DATA || UE_BUILD_DEBUG || UE_BUILD_DEVELOPMENT",
+			"WITH_SANITIZE_REFERENCES  = (WITH_EDITOR || WITH_EDITORONLY_DATA || UE_BUILD_DEBUG || UE_BUILD_DEVELOPMENT)",
 			// PIE and packaged games saves are compatible
-			"WITH_EDITOR_COMPATIBILITY = WITH_EDITOR || WITH_EDITORONLY_DATA || UE_BUILD_DEBUG || UE_BUILD_DEVELOPMENT",
+			"WITH_EDITOR_COMPATIBILITY = (WITH_EDITOR || WITH_EDITORONLY_DATA || UE_BUILD_DEBUG || UE_BUILD_DEVELOPMENT)",
 			// Enables custom delta serialization to optimize memory storage
 			// Disabled in editor builds to allow property-based structured serialization
-			"WITH_COMPACT_SERIALIZATION = !WITH_EDITOR_COMPATIBILITY",
+			"WITH_COMPACT_SERIALIZATION = !(WITH_EDITOR_COMPATIBILITY)",
 			// enable binary serialization for all archives
-			"WITH_BINARY_SERIALIZATION = !WITH_EDITOR_COMPATIBILITY",
+			"WITH_BINARY_SERIALIZATION = !(WITH_EDITOR_COMPATIBILITY)",
 			// Enable structured serialization that enables json and xml formatters
 			// Increases memory usage, do not use in release builds
-			"WITH_STRUCTURED_SERIALIZATION = WITH_EDITOR_COMPATIBILITY",
+			"WITH_STRUCTURED_SERIALIZATION = (WITH_EDITOR_COMPATIBILITY)",
 		});
 
 		if (Target.bBuildEditor)
